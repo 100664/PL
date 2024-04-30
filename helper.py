ed20bc4265b167efa720866a268aa5d454cf3d9a
jessicaclcunha <102656765+jessicaclcunha@users.noreply.github.com>
2024-04-30 20:30:05 +0100

    init

---
+++ helper.py
@@ -0,0 +1,28 @@
+"""
+
+z : prog '$'
+
+prog : Instrucao ';' prog
+    | Instrucao ';'" 
+    | Intrucao '.' prog
+
+Instrucao : parcela
+    | parcela '+' Exp
+    | parcela '-' Exp
+    
+parcela : fator
+    | fator '*' parcela
+    | fator '/' parcela
+    
+fator: termo
+    | termo '^' fator
+    
+termo: '(' Exp ')'
+    | NUM
+    
+    
+    
+z : prog '$'
+
+
+"""
--- prog1.py
+++
@@ -1,6 +0,0 @@
-import sys
-import prog1 as prog1
-
-for linha in sys.stdin:
-    prog1.rec_parser(linha)
-    
